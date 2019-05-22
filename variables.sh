ba9cc4c381e37a4a54fa357af859e51bac67a8ed
William Thorsen <william@thorsen.io>
2019-05-21 23:31:58 -0600

    Don't automatically tag a built image as `latest`

--- variables.sh
+++ variables.sh
@@ -3,5 +3,5 @@
 DOCKER_ACCOUNT_NAME=skypilot
 PACKAGE_NAME=mount-project-node
 # TODO: Read this value from `package.json`
-VERSION=1.3.2
+VERSION=1.3.3
 IMAGE_BASE_NAME=${DOCKER_ACCOUNT_NAME}/${PACKAGE_NAME}
