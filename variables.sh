9abb832ff3977fbbfb54460cfb69ed6d7bc1b54e
William Thorsen <william@thorsen.io>
2019-05-21 23:31:44 -0600

    For clarity, rename var storing Docker account name
    Rename `ACCOUNT_NAME` to `DOCKER_ACCOUNT_NAME`

--- variables.sh
+++ variables.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-ACCOUNT_NAME=skypilot
+DOCKER_ACCOUNT_NAME=skypilot
 PACKAGE_NAME=mount-project-node
 # TODO: Read this value from `package.json`
 VERSION=1.3.2
-IMAGE_BASE_NAME=${ACCOUNT_NAME}/${PACKAGE_NAME}
+IMAGE_BASE_NAME=${DOCKER_ACCOUNT_NAME}/${PACKAGE_NAME}
